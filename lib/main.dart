18202d6547b7eff66b3ece5a8da1aa2f083b8ed3
AMMA ANNORBEA OFFEI - LARBI <amma.larbi@ashesi.edu.gh>
2024-07-18 23:34:47 +0000

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quickly/Authentication/authentication_repository.dart';
+import 'package:quickly/firebase_options.dart';
 import 'package:quickly/pages/cover_page.dart';
-import 'package:quickly/pages/home_page.dart';
 
-void main() {
+void main() async {
+  // Adding Widgets binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) {
+    Get.put(AuthenticationRepository());
+  });
   runApp(const MainApp());
 }
 
@@ -11,14 +22,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'DM Sans',
-        colorScheme:ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true
-      ),
-      home: const SplashScreen()
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            fontFamily: 'DM Sans',
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true),
+        home: const SplashScreen());
   }
 }
